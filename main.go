8a8adf8ef069cdacd8735038287233deb6cffed3
Joel Nickson <joel2000nickson@gmail.com>
2021-05-21 22:20:16 +0530

    solved: json conversion | using Unmarshal

--- main.go
+++ main.go
@@ -2,60 +2,48 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
+	// "errors"
 	"fmt"
 	"log"
 	"net/http"
+	"io/ioutil"
 )
 
-type Result struct {
-	Response Response `json:"result"`
+type k struct {
+	// this must always start in capital case to export as json
+	Ok bool `json:"ok"`
 }
 
-type Response struct {
-	update_id int `json:"update_id"`
-	Message Message `json:"message"`
-}
-
-type Message struct {
-	message_id int `json:"message_id"`
-	Chat Chat `json:"chat"`
-	text string `json:"text"`
-}
-
-type Chat struct {
-	username string `json:"username"`
-}
-
-func parseIncomingRequest(r *http.Response) (*Result, error) {
-
-	var result Result
-
-	if err := json.NewDecoder(r.Body).Decode(&result); err != nil {
-		log.Printf("Error in decoding messages")
-
-		return nil, err
-	}
-
-	if result.Response.update_id == 0 {
-		log.Printf("invalid update_id, got update id = 0")
-		return nil, errors.New("SERVER ERROR")
-	}
-
-	return &result, nil
-}
 
 func main() {
 
-	httpreq, err := http.Get("https://api.telegram.org/bot1815331593:AAGM_U2Dw5KxQo3rjTIajSesZvfcj9r_iYw/getUpdates?limit=1")
+	url := "https://raw.githubusercontent.com/Co-Science/tele-go-m/Joel-Nickson-patch-1/test.json"
+
+	resp, err := http.Get(url)
 	if err != nil {
-		log.Printf("Error in rerieving request")
-	}
-	
-	parsedData, err := parseIncomingRequest(httpreq)
-	if err != nil {
-		fmt.Println("Error in parsing retreived data!")
+	   log.Fatalln(err)
 	}
 
-	fmt.Println(parsedData.Response.Message)
+	 body, err := ioutil.ReadAll(resp.Body)
+   if err != nil {
+      log.Fatalln(err)
+   }
+	//Convert the body to type string
+   sb := "["+string(body)+"]"
+
+//sb := "[{\"ok\": true}]"
+   fmt.Println(sb)
+   bytes := []byte(sb)
+ 	
+   var json_in_go []k
+   json.Unmarshal(bytes, &json_in_go)
+
+
+   fmt.Println(json_in_go)
+   for l := range json_in_go {
+        fmt.Printf("Id = %v ", json_in_go[l].Ok)
+        fmt.Println()
+    }
+
+fmt.Println("\n\n:)")
 }
