dda666f7b102d112f053b34a0f10e48500e56b7a
joel <joel2000nickson@gmail.com>
2021-05-31 00:56:29 +0530

    change form jsonparser to struct as it was not working as intended

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -10,7 +11,37 @@ import (
 	"github.com/buger/jsonparser"
 )
 
-func sendMessages(message string) (error){
+type JsonToGo struct {
+	Ok     bool     `json:"ok"`
+	Result []Result `json:"result"`
+}
+type From struct {
+	ID           int    `json:"id"`
+	IsBot        bool   `json:"is_bot"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Username     string `json:"username"`
+	LanguageCode string `json:"language_code"`
+}
+type Chat struct {
+	ID                          int    `json:"id"`
+	Title                       string `json:"title"`
+	Type                        string `json:"type"`
+	AllMembersAreAdministrators bool   `json:"all_members_are_administrators"`
+}
+type Message struct {
+	MessageID    int    `json:"message_id"`
+	From         From   `json:"from"`
+	Chat         Chat   `json:"chat"`
+	Date         int    `json:"date"`
+	NewChatTitle string `json:"new_chat_title"`
+}
+type Result struct {
+	UpdateID int     `json:"update_id"`
+	Message  Message `json:"message"`
+}
+
+func sendMessages(message string) error {
 
 	url := fmt.Sprintf("https://api.telegram.org/bot<token>/sendMessage?chat_id=856051391&text=%s", message)
 
@@ -29,19 +60,30 @@ func sendMessages(message string) (error){
 	}
 }
 
-func parseIncomingRequest(httpResp *http.Response) (string, error) {
+func parseIncomingRequest(httpResp *http.Response) {
 
 	bodyBytes, err := ioutil.ReadAll(httpResp.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	body, err := jsonparser.GetString(bodyBytes, "result", "[0]", "message", "text")
-	if err != nil {
-		log.Fatal("Error in parsing data")
+	sb := "[" + string(bodyBytes) + "]"
+
+	fmt.Println(sb)
+	bytes := []byte(sb)
+
+	var json_in_go []JsonToGo
+	json.Unmarshal(bytes, &json_in_go)
+
+	fmt.Println(json_in_go)
+	for _, v := range json_in_go {
+		fmt.Printf("\nId = %v \n", v)
+		for j, v := range v.Result {
+			fmt.Printf("unk= %v,%v \n", j, v)
+		}
+		fmt.Println()
 	}
 
-	return body, nil
 }
 
 func main() {
@@ -50,13 +92,15 @@ func main() {
 	if err != nil {
 		log.Printf("Error in rerieving request")
 	}
-	
-	parsedData, err := parseIncomingRequest(httpreq)
+
+	parseIncomingRequest(httpreq)
+
+	// parsedData, err := parseIncomingRequest(httpreq)
 	if err != nil {
 		fmt.Println("Error in parsing retreived data!")
 	}
 
-	fmt.Println(parsedData)
+	// fmt.Println(parsedData)
 
-	sendMessages(parsedData)
+	sendMessages("hello")
 }
