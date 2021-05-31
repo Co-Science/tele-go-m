8986926acc22f824e56f35a4ceb513071525b7aa
joel <joel2000nickson@gmail.com>
2021-06-01 00:24:26 +0530

    make use of the previously created variables to send msg

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/buger/jsonparser"
 )
@@ -41,9 +44,9 @@ type Result struct {
 	Message  Message `json:"message"`
 }
 
-func sendMessages(message string) error {
+func sendMessages(message string, token string, chat_id string) error {
 
-	url := fmt.Sprintf("https://api.telegram.org/bot<token>/sendMessage?chat_id=856051391&text=%s", message)
+	url := fmt.Sprintf("https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chat_id+"&text=%s", message)
 
 	httpSend, err := http.Get(url)
 	if err != nil {
@@ -86,9 +89,38 @@ func parseIncomingRequest(httpResp *http.Response) {
 
 }
 
+func fileReader(filename string) []string {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatalf("failed to open")
+
+	}
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+	var text []string
+
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+
+	file.Close()
+
+	secreats := make([]string, 2)
+	for i, each_ln := range text {
+		secreats[i] = strings.Split(each_ln, "=")[1]
+	}
+	return secreats
+}
+
 func main() {
 
-	httpreq, err := http.Get("https://api.telegram.org/bot<token>/getUpdates?limit=1")
+	secreats := fileReader("env.txt")
+	token, chat_id := secreats[0], secreats[1]
+	fmt.Println(token, chat_id)
+
+	httpreq, err := http.Get("https://api.telegram.org/bot" + token + "/getUpdates?limit=1")
 	if err != nil {
 		log.Printf("Error in rerieving request")
 	}
@@ -102,5 +134,5 @@ func main() {
 
 	// fmt.Println(parsedData)
 
-	sendMessages("hello")
+	sendMessages("hello", token, chat_id)
 }
